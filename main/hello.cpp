f5051ebca385dacff953cbf4020bb571984a6963
roshankumar0402 <76490400+roshankumar0402@users.noreply.github.com>
2023-02-16 22:22:37 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run it smoothly\n";
+  cout << "I have successfully built and run it and then UPDATED it\n";
   return 0;
 }
